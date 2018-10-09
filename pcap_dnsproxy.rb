fc2a641f2a67366a7e2126d4ec603ebdf12c9ef7
zhenhappy <q505507538@gmail.com>
2018-10-09 10:19:39 +0800

    update Pcap_DNSProxy

--- pcap_dnsproxy.rb
+++ pcap_dnsproxy.rb
@@ -1,16 +1,16 @@
 class PcapDnsproxy < Formula
   desc "Pcap_DNSProxy, a local DNS server based on WinPcap and LibPcap"
   homepage "https://github.com/chengr28/Pcap_DNSProxy"
-  url "https://github.com/chengr28/Pcap_DNSProxy/releases/download/v0.4.9.8/Pcap_DNSProxy-0.4.9.8-bin.zip"
-  version "0.4.9.8"
-  sha256 "6d7b25b2bc719bac0e37be5ab890f282f1f4cc76470fbcc4e0e5ad49ab11a834"
+  url "https://github.com/chengr28/Pcap_DNSProxy/releases/download/v0.4.9.11/Pcap_DNSProxy-0.4.9.11-bin.zip"
+  version "0.4.9.11"
+  sha256 "f446c215d77c9bdbd11e66679fa1ed76270873e4589fc637086a655d3fa85c5f"
 
   def install
     bin.install "macOS/Pcap_DNSProxy"
     (etc/"pcap_dnsproxy").install Dir["macOS/*.{conf,sh,ini,txt}"]
     prefix.install "macOS/pcap_dnsproxy.service.plist"
   end
-  
+
   plist_options :startup => true, :manual => "sudo #{HOMEBREW_PREFIX}/opt/pcap_dnsproxy/bin/Pcap_DNSProxy -c #{HOMEBREW_PREFIX}/etc/pcap_dnsproxy/"
 
   test do
